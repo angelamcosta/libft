14fe7d6d05ddc175766e8d912d5601e929504ef6
Angela Lima <angelamcostalima@icloud.com>
2022-08-15 12:12:25 +0100

    minor fix in ft_split ft_alloc func

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 12:35:58 by anlima            #+#    #+#             */
-/*   Updated: 2022/08/15 11:55:07 by anlima           ###   ########.fr       */
+/*   Updated: 2022/08/15 12:11:38 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_alloc(char *s, int i, int j, char sep)
+char	*ft_alloc(char *s, int i, int j)
 {
 	char	*str;
 	int	k;
@@ -22,11 +22,7 @@ char	*ft_alloc(char *s, int i, int j, char sep)
 	if (!str)
 		return (0);
 	while (j < i)
-	{
-		if (s[j] != sep)
-			str[k++] = s[j];
-		j++;
-	}
+		str[k++] = s[j++];
 	str[k] = '\0';
 	return (str);
 }
@@ -50,7 +46,7 @@ char	**ft_split(char const *s, char c)
 		{
 			if (s[i + 1] == '\0')
 				i++;
-			strl[k++] = ft_alloc((char *)s, i, j, c);
+			strl[k++] = ft_alloc((char *)s, i, j);
 			j = i + 1;
 		}
 	}
@@ -63,6 +59,7 @@ char	**ft_split(char const *s, char c)
 int	main(void)
 {
 	char **strl = ft_split("guga,lela,clari,gonas", ',');
-	printf("%s", strl[0]);
+	for (int i = 0; i < 4; i++)
+		printf("%s", strl[i]);
 	free(strl);
 }
