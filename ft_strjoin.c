a79152c2ab00b11c6df0bdcbfd06c0cb599cef95
Angela Lima <angelamcostalima@icloud.com>
2022-10-16 16:20:00 +0100

    refactor strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,29 +6,25 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 11:56:00 by anlima            #+#    #+#             */
-/*   Updated: 2022/10/11 09:53:57 by anlima           ###   ########.fr       */
+/*   Updated: 2022/10/16 16:16:37 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	add_str(char *joined, char *s, int i)
-{
-	while (*s)
-		joined[i++] = *s++;
-	return (i);
-}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*joined;
 	int		i;
 
+	i = 0;
 	joined = malloc(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
 	if (!joined)
 		return (NULL);
-	i = add_str(joined, (char *)s1, 0);
-	i = add_str(joined, (char *)s2, i);
+	while (joined[i] && *s1)
+		joined[i++] = *s1++;
+	while (joined[i] && *s2)
+		joined[i++] = *s2++;
 	joined[i] = '\0';
 	return (joined);
 }
