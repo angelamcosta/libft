e2af6472f809bc689be32fef6824db646efc806a
Angela Lima <angelamcostalima@icloud.com>
2022-10-27 15:29:25 +0100

    fix calloc maybe?

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 09:33:27 by anlima            #+#    #+#             */
-/*   Updated: 2022/10/26 15:38:55 by anlima           ###   ########.fr       */
+/*   Updated: 2022/10/27 15:28:54 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	*ft_calloc(size_t num, size_t size)
 {
 	void	*arr;
 
-	if (2147483647 / size < num)
-		return (NULL);
 	arr = (void *)malloc(size * num);
 	if (!arr)
 		return (NULL);
-	ft_bzero(arr, size * num);
+	ft_memset(arr, size * num);
 	return (arr);
 }
